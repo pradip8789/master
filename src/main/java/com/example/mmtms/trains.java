ab81b524b671b7974c7be1bfd7ea9c99e8b3a27f
pradip8789 <pradip8789@gmail.com>
2023-12-28 16:52:22 +0530

    adding trains module

---
+++ src/main/java/com/example/mmtms/trains.java
@@ -0,0 +1,14 @@
+
+
+package com.example.mmtms;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class trains {
+    @GetMapping ("/trains")
+    public String getData() { return "Please book trains ticket from MMT at 30 % discount" ; }
+
+}
